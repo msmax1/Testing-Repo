0e72a578166c8282d0b537aedebaef8365b91cf6
maxuser1 <matzjan51@gmail.com>
2022-05-31 19:01:09 -0500

    New version of the code - May 31st

--- DemoProjectGit/src/com/company/clasemain/MainClass.java
+++ DemoProjectGit/src/com/company/clasemain/MainClass.java
@@ -1,8 +1,12 @@
 package com.company.clasemain;
 
+import com.company.test.TestingClass2;
+
 public class MainClass {
     public static void main(String[] args) {
-        TestingClass test = new TestingClass();
+        TestingClass2 test = new TestingClass2();
         test.testingMethod();
+        System.out.println("This a new test");
+        System.out.println("This a new testing proyect");
     }
 }
--- DemoProjectGit/src/com/company/test/TestingClass2.java
+++ DemoProjectGit/src/com/company/test/TestingClass2.java
@@ -1,5 +1,8 @@
 package com.company.test;
 
 public class TestingClass2 {
+    public void testingMethod() {
+        System.out.println("Hello world");
+    }
     //Just a demo class for git
 }
