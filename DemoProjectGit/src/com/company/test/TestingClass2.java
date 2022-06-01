771c8cbacdaf0ee310c0e586c71bcd58434b3fea
maxuser1 <matzjan51@gmail.com>
2022-05-31 19:18:19 -0500

    New changes

--- DemoProjectGit/src/com/company/test/TestingClass2.java
+++ DemoProjectGit/src/com/company/test/TestingClass2.java
@@ -4,6 +4,7 @@ public class TestingClass2 {
     public void testingMethod() {
         System.out.println("Hello world");
         System.out.println("New line added in testing method");
+        System.out.println("Nuevo proyecto clonado");
     }
     //Just a demo class for git
 }
